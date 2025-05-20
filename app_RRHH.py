78117f83c7e7b45199050c3a4a220b8e33a564bf
CaroCodina <31101877+CaroCodina@users.noreply.github.com>
2025-05-20 13:14:05 -0300

    Update app_RRHH.py

--- app_RRHH.py
+++ app_RRHH.py
@@ -234,4 +234,5 @@ def eliminar_candidato(id):
     return redirect(url_for('candidatos'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
